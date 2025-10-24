1305182a09b3e07bb8405871add7243254df21b3
Ejf25441817 <ernestojavierfortini@gmail.com>
2025-10-23 21:44:01 -0300

    creacion de las app blog y comentarios mas creacion de archivos para cada app y agregado de carpetas para el html

---
+++ instituto/apps/comentarios/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import get_object_or_404 ,redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Comentario
+from .forms import ComentarioForm
+from apps.blog.models import Articulo
+from .mixins import ComentarioPermisoMixin
+
+
+
+class ComentarioCreateView(LoginRequiredMixin, CreateView):
+    model = Comentario
+    form_class = ComentarioForm
+
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        form.instance.articulo = get_object_or_404(
+            Articulo, pk=self.kwargs['articulo_id']
+        )
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy(
+            'blog:detalle_articulo',
+            kwargs={'pk': self.kwargs['articulo_id']}
+        )
+
+    # Evitar acceso directo vía GET
+    def get(self, request, *args, **kwargs):
+        return redirect('blog:detalle_articulo', pk=self.kwargs['articulo_id'])
+
+
+
+class ComentarioUpdateView(LoginRequiredMixin, ComentarioPermisoMixin, UpdateView):
+    model = Comentario
+    form_class = ComentarioForm
+    template_name = 'comentarios/editar_comentario.html'
+
+
+class ComentarioDeleteView(LoginRequiredMixin, ComentarioPermisoMixin, DeleteView):
+    model = Comentario
+    template_name = 'comentarios/eliminar_comentario.html'
+
--- instituto/apps/usuarios/views.py
+++ instituto/apps/usuarios/views.py
@@ -1,3 +1,94 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth import login, logout
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegistroUsuarioForm,PerfilUsuarioForm
+from .models import Usuario
 
-# Create your views here.
+#registro de usuario
+class RegistroUsuarioView(View):
+    form_class = RegistroUsuarioForm
+    template_name = 'usuarios/registro.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        form = self.form_class(request.Post)
+        if form.is_valid():
+            usuario = form.save()
+            login(request, usuario)
+            messages.success(request, f"Bienvenido {usuario.username}! Tu cuenta ha sido creada exitosamente.")
+            return redirect('index')
+        return render(request, self.template_name, {'form': form})
+    
+class LoginUsuarioView(View):
+    template_name = 'usuarios/login.html'
+
+    def get(self, request):
+        form = AuthenticationForm()
+        form.fields['username'].label = "Usuario"
+        form.fields['username'].widget.attrs.update({'class': 'input-text', 'id': 'id_username','placeholder': 'Usuario'})
+        form.fields['password'].widget.attrs.update({'class': 'input-text', 'id': 'id_password','placeholder': 'Contraseña'})
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        form.fields['username'].label = "Usuario"
+        form.fields['username'].widget.attrs.update({'class': 'input-text', 'id': 'id_username','placeholder': 'Usuario'})
+        form.fields['password'].widget.attrs.update({'class': 'input-text', 'id': 'id_password','placeholder': 'Contraseña'})
+
+        if form.is_valid():
+            usuario = form.get_user()
+            login(request, usuario)
+            messages.success(request, f"Bienvenido {usuario.username}.")
+            return redirect('index')
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos.")
+        
+        return render(request, self.template_name, {'form': form})
+    
+class PerfilDetalleView(LoginRequiredMixin, TemplateView):
+    template_name = 'usuarios/perfil.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['usuario'] = self.request.user
+        return context
+    
+class PerfilUsuarioView(LoginRequiredMixin, UpdateView):
+    model = Usuario
+    form_class = PerfilUsuarioForm
+    template_name = 'usuarios/editar_perfil.html'
+    success_url = reverse_lazy('usuarios:perfil')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+    
+    def form_valid(self, form):
+        messages.success(self.request, "Tu perfil ha sido actualizado exitosamente.")
+        return super().form_valid(form)
+    
+class EliminarUsuarioView(LoginRequiredMixin,View):
+    def get(self, request):
+        return render(request, 'usuarios/eliminar_perfil.html')
+    
+    def post(self, request):
+        usuario = request.user
+        logout(request)
+        usuario.delete()
+        messages.success(request, "Tu cuenta ha sido eliminada exitosamente.")
+        return redirect('index')
+    
+class LogoutUsuarioView(View):
+    template_name = 'usuarios/logout.html'
+
+    def post(self, request):
+        logout(request)
+        messages.success(request, "Has cerrado sesión exitosamente.")
+        return redirect('index')
